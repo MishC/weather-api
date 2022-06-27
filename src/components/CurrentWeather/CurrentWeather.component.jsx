d2d7f9cf1a23af3977d08454ada170321a34e5fb
MishC <michaela.chovancova@gmail.com>
2022-06-27 21:49:09 +0200

    Current weather component

--- src/components/CurrentWeather/CurrentWeather.component.jsx
+++ src/components/CurrentWeather/CurrentWeather.component.jsx
@@ -6,14 +6,18 @@ import SearchBar from "../SearchBar/SearchBar.component";
 import "./Current.Weather.styles.css";
 
 export default class CurrentWeather extends React.Component {
-  state = {
-    instantWeather: {},
-    city: "",
-    district: "",
-    lat: 0,
-    lon: 0,
-    // searchField:""
-  };
+  constructor() {
+    super();
+    this.state = {
+      instantWeather: {},
+      instantTemperature: 0,
+      city: "",
+      lat: 0,
+      lon: 0,
+      ready: false,
+      // searchField:""
+    };
+  }
 
   /*function handleDefault(response) {
    setDefaultCity(response.data.city.name);
@@ -25,65 +29,80 @@ export default class CurrentWeather extends React.Component {
     this.setState({ lat: lat });
     this.setState({ lon: lon });
 
-    const url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`;
-    const url1 = `https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${lat}&lon=${lon}`;
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`;
+    let url1 = `https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${lat}&lon=${lon}`;
     axios.get(url).then((res) => {
       const data = res.data;
       this.setState({
-        instantWeather: data.properties.timeseries[0].data.instant.details,
+        instantTemperature:
+          data.properties.timeseries[0].data.instant.details.air_temperature,
       });
     });
     axios.get(url1).then((res) => {
       const data1 = res.data.features[0].properties.address;
-      this.setState({ city: data1.city });
-      this.setState({ district: data1.city_district });
+      this.setState({ city: data1.city + ", " + data1.city_district });
     });
   };
-  /* handleSearch(event) {
-    event.preventDefault();
-    this.setState({city:event.target.value});
-  }
-handleSubmit(event) {
+  ///////
+  handleSearch = (event) => {
+    // event.preventDefault();
+    this.setState({ city: event.target.value });
+    //console.log(this.state.city);
+  };
+  handleSubmit = (event) => {
     event.preventDefault();
 
-    let urlCity = `https://api.openweathermap.org/data/2.5/forecast/?q=${this.state.city}&units=metric&APPID=${apiKey}`;
+    let urlCity = `https://nominatim.openstreetmap.org/search?q=${this.state.city}&format=geojson`;
 
-    axios.get(urlCity).then(handleResponse);
-  }
- handleResponse(response) {
+    axios.get(urlCity).then(this.handleResponse);
+  };
+  handleResponse = (response) => {
     let now = new Date();
 
+    console.log(response.data);
     this.setState({
       ready: true,
-      city: response.data.city.name.toUpperCase(),
-      country: response.data.city.country,
-      temperature: Math.round(response.data.list[0].main.temp),
-      icon: response.data.list[0].weather[0].icon,
-      // icon: `https://openweathermap.org/img/wn/${response.data.list[0].weather[0].icon}@2x.png`,
-      description: response.data.list[0].weather[0].description,
-      precipitation: Math.round(response.data.list[0].pop),
-      wind: Math.round(response.data.list[0].wind.speed),
-      humidity: response.data.list[0].main.humidity,
-      windDeg: Math.round(response.data.list[0].wind.deg),
-      date: currentDate(now),
-      coordinates: response.data.city.coord,
+      city: response.data.features[0].properties.display_name,
+      lat: response.data.features[0].geometry.coordinates[0].toFixed(5),
+      lon: response.data.features[0].geometry.coordinates[1].toFixed(5),
     });
-  }*/
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${this.state.lat}&lon=${this.state.lon}`;
+    axios.get(url).then((res) => {
+      const data = res.data;
+      console.log(res);
+      let avg_temperature = 0;
+      let length_timeseries = data.properties.timeseries.length;
+      data.properties.timeseries.forEach((timeserie) => {
+        avg_temperature += timeserie.data.instant.details.air_temperature;
+      });
+      this.setState({
+        instantTemperature: avg_temperature / length_timeseries,
+      });
+      console.log("hello");
+      console.log(data.properties.timeseries[0].data.instant.details);
+    });
+  };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(this.retrievePosition);
+    console.log("Mount");
   }
-
+  /*componentDidUpdate({city}){
+if
+}*/
   render() {
     if (this.state.lat !== 0) {
       return (
         <div className="current-weather">
           <h2>City: {this.state.city}</h2>
-          <h2>City District: {this.state.district}</h2>
 
-          <SearchBar />
+          <SearchBar
+            handleSearch={this.handleSearch}
+            handleSubmit={this.handleSubmit}
+            handleResponse={this.handleResponse}
+          />
 
-          <h2>Temperature: {this.state.instantWeather.air_temperature} °C</h2>
+          <h2>Temperature: {this.state.instantTemperature} °C</h2>
         </div>
       );
     } else {
